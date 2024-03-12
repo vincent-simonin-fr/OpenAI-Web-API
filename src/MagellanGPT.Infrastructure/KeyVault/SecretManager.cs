0b4e620e81e443b88e092bf4bf75409d97a8a1a5
twentycent <Vincent.Simonin@diiage.org>
2024-03-12 13:31:11 +0100

    Ajout keyvault et refactoring embedding ai search

---
+++ src/MagellanGPT.Infrastructure/KeyVault/SecretManager.cs
@@ -0,0 +1,20 @@
+﻿namespace MagellanGPT.Infrastructure.KeyVault;
+
+public class SecretManager
+{
+    private static SecretManager _instance;
+
+    public string OpenAiKey { get; set;}
+    public string AiSearchKey { get; set; }
+    public string CosmosDbKey { get; set; }
+
+    public static SecretManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SecretManager();
+        }
+        return _instance;
+    }
+}
+
